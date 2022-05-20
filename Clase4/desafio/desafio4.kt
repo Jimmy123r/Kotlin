ef8d03293e1db5ab04e52b0376e34d17568dc685
Jimmy dario rojas <90005667+Jimmy123r@users.noreply.github.com>
2022-05-20 18:55:57 -0300

    Create desafio4.kt

---
+++ Clase4/desafio/desafio4.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    var semana = 6
+    
+    when(semana){
+        1 -> println("Lunes")
+        2 -> println("Martes")
+        3 -> println("Miercoles")
+        4 -> println("Jueves")
+        5 -> println("Viernes")
+        6 -> println("Sabado")
+        7 -> println("Domingo")
+        else -> println("Error")
+    }
+}
