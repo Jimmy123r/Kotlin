a6e8e638e62c602d94a8cb1b32603816be40722b
Jimmy dario rojas <90005667+Jimmy123r@users.noreply.github.com>
2022-05-20 19:05:42 -0300

    Create Desafio5.kt

---
+++ Clase5/Desafio5/Desafio5.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+   var i = 1
+    while (i <= 5){
+        if(i == 2 || i == 4)
+            println("este numero es par = $i")
+        else
+            println("este numero es impar = $i")
+        i++
+    }
+}
