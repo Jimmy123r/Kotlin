d8c461bd683bfddae1253b68963d361a7ec77af0
Jimmy dario rojas <90005667+Jimmy123r@users.noreply.github.com>
2022-05-20 18:45:28 -0300

    Create desafio3.kt

---
+++ Clase3/desafio3/desafio3.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+   var num1 = 100
+    var num2 = 200
+    if(num1 >= num2)
+        println("el $num1 es el numero mas grande , que ell numero $num2")
+    else
+        println("el $num2 es el numero mas grande , que el numero $num1")
+}
+
+
